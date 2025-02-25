4172425206bb3df6dceb7ac851f4d979822f4f79
Diones <dionesforteski@gmail.com>
2025-02-25 00:01:51 -0300

    Primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import '../cls/auth_service.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final AuthService _authService = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Login com Google")),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () async {
+            final user = await _authService.signInWithGoogle();
+            if (user != null) {
+              print("Usuário logado: ${user.displayName}");
+            }
+          },
+          child: Text("Login com Google"),
+        ),
+      ),
+    );
+  }
+}
